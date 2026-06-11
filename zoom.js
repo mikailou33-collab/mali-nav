a4e2888c3907d3e6f3025e939faedbdc03d8944f
Mikailou <mikailou33@gmail.com>
2026-06-11 21:27:54 +0300

    OpenStreetMap iframe Tombouctou

--- zoom.js
+++ zoom.js
@@ -1,8 +1,8 @@
 const fs=require('fs');
 var c=fs.readFileSync('region-tombouctou.html','utf8');
-var newMap='<div class="map-wrap"><iframe src="https://www.openstreetmap.org/export/embed.html?bbox=-4.5,16.0,2.5,25.0&layer=mapnik" style="width:100%;height:100%;border:none;" allowfullscreen loading="lazy"></iframe></div>';
-var start=c.indexOf('<div class="map-wrap">');
-var end=c.indexOf('</div>',start)+6;
-c=c.slice(0,start)+newMap+c.slice(end);
+var svgStart=c.indexOf('<svg width="100%"');
+var svgEnd=c.indexOf('</svg>',svgStart)+6;
+var iframe='<iframe src="https://www.openstreetmap.org/export/embed.html?bbox=-4.5,16.0,2.5,25.0&layer=mapnik" style="width:100%;height:100%;border:none;min-height:400px;" allowfullscreen loading="lazy"></iframe>';
+c=c.slice(0,svgStart)+iframe+c.slice(svgEnd);
 fs.writeFileSync('region-tombouctou.html',c);
 console.log('OK fait');
